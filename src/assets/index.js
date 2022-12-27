50410d94a8c7cbdd14ed98f0040cb73d5aa85d14
vanlinh1330 <levanlinh1330@gamil.com>
2022-12-28 01:15:14 +0700

    Xu ly brand, whatGPT3, feature

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,11 @@ const images = {
     logo2: require('./images/logo.svg'),
     people: require('./images/people.png'),
     ai: require('./images/ai.png'),
+    google: require('./images/google.png'),
+    slack: require('./images/slack.png'),
+    atlassian: require('./images/atlassian.png'),
+    dropbox: require('./images/dropbox.png'),
+    shopify: require('./images/shopify.png'),
 };
 
 export default images;
--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './brand.css'
+import React from 'react';
+import './brand.css';
+import images from '../../assets';
 
 const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+    return (
+        <div className="gpt3_brand">
+            <div>
+                <img src={images.google} alt="google" />
+            </div>
+            <div>
+                <img src={images.slack} alt="slack" />
+            </div>
+            <div>
+                <img src={images.atlassian} alt="atlassian" />
+            </div>
+            <div>
+                <img src={images.dropbox} alt="dropbox" />
+            </div>
+            <div>
+                <img src={images.shopify} alt="shopify" />
+            </div>
+        </div>
+    );
+};
 
-export default Brand
+export default Brand;
